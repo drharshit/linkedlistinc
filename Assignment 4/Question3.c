fe513072fb88982b242daa8ce5d9e9d5decff662
hardik-gupta <44121166+hardik-gupta@users.noreply.github.com>
2020-10-31 23:32:37 +0530

    Create Question3.c

---
+++ Assignment 4/Question3.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<string.h>
+
+int size, top = -1;
+char stack[20];
+char postfix[20];
+int j=0;
+
+//Pushing the element
+void push(char ch){
+    top = top+1;
+    stack[top] = ch;
+}
+
+//Top element
+char peek(){
+    if(top==-1)
+        printf("Underflow\n");
+    else
+        return stack[top];
+}
+
+
+//Popping the element
+char pop(){
+    return stack[top--];
+}
+
+//If stack is empty or not
+int isStackEmpty(){
+    if(top == -1)
+        return -1;
+    return 0;
+}
+
+//Deciding the precedence
+int Prec(char ch){
+    if(ch=='+' || ch=='-')
+            return 1;
+    else if(ch=='*' || ch=='/')
+            return 2;
+    return 0;
+}
+
+//Infix to Postfix expression
+void intopost(char* infix){
+    for(int i=0; i<size; i++){
+            if(infix[i] >= 'a' && infix[i] <= 'z' || infix[i] >= 'A' && infix[i] <= 'Z')  //If it's an operand
+                postfix[j++] = infix[i];
+            
+            else if(infix[i] == '(')                 // If it is what it is
+                push(infix[i]);
+
+            else if(infix[i] == ')'){                // If it is what it is
+                    while(peek() != '(')
+                        postfix[j++] = pop();
+                    pop();
+            }
+
+            else{                                    // If it's not what we thought it was
+                while( !isStackEmpty() && (Prec(infix[i]) <= Prec(peek())))
+                    postfix[j++] = pop();
+                push(infix[i]);
+            }
+    }
+    while(!isStackEmpty())
+        postfix[j++] = pop();
+}
+
+int main(){
+    char infix[20];
+    printf("Infix Expression: ");
+    scanf("%s", infix);
+    size = strlen(infix);
+    intopost(infix);
+    printf("Postfix Expression: %s\n",postfix);
+    return 0;
+}
